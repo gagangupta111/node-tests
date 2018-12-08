61bf1e9ebab4949d6ff21d4ec75acaa12d4345da
GaganGupta <gagan_gupta111@yahoo.com>
2018-12-08 15:30:00 +0530

    added more test functions

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,18 @@
 module.exports.add = (a,b) => a+b;
 
-module.exports.sub = (a,b) => {a-b};
+module.exports.sub = (a,b) => {
+     a-b;
+};
 
 module.exports.returnSub = (a,b) => {
     return a-b;
 };
 
+module.exports.setName = (user, names) => {
+    var arrayNames = names.split(' ');
 
+    user.firstName = arrayNames[0];
+    user.lastName = arrayNames[1];
+
+    return user;
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,20 +1,19 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('Should add 2 numbrs', () => {
     const res = utils.add(3,2);
 
-    if(res != 5){
-        throw new Error(`Expected 5 but got ${res}`);
-    }
+    expect(res).toExist('Should Exist!');
+    expect(res).toEqual(5).toBeA('number');
 
 })
 
 it('Should sub 2 numbrs', () => {
     const res = utils.sub(3,2);
 
-    if(res != 1){
-        throw new Error(`Expected 1 but got ${res}`);
-    }
+    expect(res).toNotExist('Result Should be undefined');
     
 });
 
@@ -22,8 +21,59 @@ it('Should sub 2 numbrs', () => {
 it('Should sub 2 numbrs and return ', () => {
     const res = utils.returnSub(3,2);
 
-    if(res != 1){
-        throw new Error(`Expected 1 but got ${res}`);
-    }
+    expect(res).toExist('Should Exist!');
+    expect(res).toEqual(1, 'result Should be 1').toBeA('number');
     
-})
+});
+
+it('test equal and tobe', () => {
+
+    expect({name: "Gagan"}).toEqual({name: "Gagan"});
+
+});
+
+it('test include', () => {
+
+    expect([1,2,3,4]).toInclude(4, 'Should have 4 also');
+
+});
+
+it('test include property', () => {
+
+    expect({
+        name : "gagan",
+        age: 29,
+        address: "India"
+    }).toInclude({
+        age: 29
+    }, 'Should have age : 29 ');
+
+});
+
+
+it('test exclude property', () => {
+
+    expect({
+        name : "gagan",
+        age: 29,
+        address: "India"
+    }).toExclude({
+        age: 26
+    }, 'Should not have age : 26 ');
+
+});
+
+it('test setName function', () => {
+
+    var user = {name: "Gagan Gupta", age: 29};
+    var fullName = "Gagan Gupta";
+    
+    fullName = utils.setName(user, fullName);
+    
+    expect(user).toEqual(fullName, "Both variables should have equal values");
+    expect(user).toInclude({
+        firstName: 'Gagan',
+        lastName: 'Gupta'
+    }, "Should include name properties also");
+
+});
